60d98c617ba494d17e16dbb0d2e09a396be8e701
Essam Zaki <esoozaki2@gmail.com>
2023-07-07 14:38:19 +0300

    ..

---
+++ Carea/obj/Release/net6.0/PubTmp/Out/wwwroot/js/push.js
@@ -0,0 +1,59 @@
+﻿$(document).ready(() => {
+	$("#sub").click(() => {
+		let obj =
+		{
+			deviceId: $("#DeviceId").val(),
+			isAndroiodDevice: true,
+			title: $("#Title").val(),
+			body: $("#Body").val()
+		}
+        console.log(obj);
+		$.ajax({
+			method: 'POST',
+			url: '/Notafication/send',
+			data: obj,
+			success: function (res) {
+				console.log(res);
+			}
+		})
+
+	})
+
+})
+
+
+/*----------------------------------------*/
+$(document).ready(() => {
+	$("#Predict").click(() => {
+		let obj =
+		{
+			doors: $("#Doors").val(),
+			wheel: $("#Wheel").val(),
+			levy: $("#Levy").val(),
+			engine_volume: $("#EngineVolume").val(),
+			mileage: $("#Mileage").val(),
+			cylinders: $("#Cylinders").val(),
+			airbags: $("#Airbags").val(),
+			model: $("#Model").val(),
+			category: $("#Category").val(),
+			leather_interior: $("#LeatherInterior").val(),
+			fuel_type: $("#FuelType").val(),
+			gear_box_type: $("#GearBoxType").val(),
+			drive_wheels: $("#DriveWheels").val(),
+			engine_turbo: $("#EngineTurbo").val(),
+			age: $("#Age").val(),
+			manufacturer: $("#Manufacturer").val(),
+		}
+		console.log(obj);
+		$.ajax({
+			method: 'POST',
+			url: 'https://car-price-prediction-e99b.onrender.com/predict',
+			data: obj,
+			success: function (res) {
+				console.log(res);
+			}
+		})
+
+	})
+
+})
